36105b8fe94f9c89787413fb361fbff7fd988b4a
Administrator <Administrator@ZC7BV9ATZWZIN2C>
2015-10-10 17:28:43 +0800

    一些用法

---
+++ xhsp/WebContent/script/regchechdata.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+function chechdata() {
+	var ssn = form.username.value.toLowerCase();
+	if (!che) {
+
+	}
+
+}
+
+function checkUseName(ssn) {
+	if (ssn.length < 6 || ssn.length > 18) {
+		alert("\请输入正确的用户名，用户名长度为3-18位！")
+	}
+}
